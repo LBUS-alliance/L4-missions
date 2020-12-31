07af3cd2749e689714e6bddf95afc1a70f8619c6
Mikayla <mikayla61220@gmail.com>
2020-12-30 18:59:56 -0800

    add copy to clipboard

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
-var toggleColorMode = () => {
+const copyFit = id => {
+    var text = document.getElementById(id).innerHTML;
+    navigator.clipboard.writeText(text);
+
+    $(`#tooltip-${id}`).html('Copied!');
+};
+const tooltipHoverIn = id => () => {
+    let top = document.getElementById(`copy-${id}`).getBoundingClientRect().top - document.body.getBoundingClientRect().top - 32;
+    let left = document.getElementById(`copy-${id}`).getBoundingClientRect().left - 50;
+    $(`#tooltip-${id}`).css({ top: top, left: left, position: 'absolute' });
+    $(`#tooltip-${id}`).show();
+};
+const tooltipHoverOut = id => () => {
+    $(`#tooltip-${id}`).html('Copy to clipboard');
+    $(`#tooltip-${id}`).hide();
+}
+
+const toggleColorMode = () => {
+    // replace CSS file
     const oldCSS = document.getElementsByTagName("link").item(0);
     const oldFile = oldCSS.href.split("/").slice(-1)[0];
-
     var newCSS = document.createElement("link");
     newCSS.setAttribute("rel", "stylesheet");
     newCSS.setAttribute("type", "text/css");
     newCSS.setAttribute("href", oldFile === "styles_dark.css" ? "styles_light.css" : "styles_dark.css");
-
     document.getElementsByTagName("head").item(0).replaceChild(newCSS, oldCSS);
 
-    $('#color-mode').html(`<img src="imgs/${oldFile === "styles_dark.css" ? 'sun' : 'moon'}.svg"/>`);
+    // toggle color-mode icon
+    $('#color-mode').html(`<img src="imgs/${oldFile === 'styles_dark.css' ? 'sun' : 'moon'}.svg"/>`);
+
+    // toggle clipboard icons
+    $('.clipboard').attr('src', oldFile === 'styles_dark.css' ? 'imgs/clipboard-dark.svg' : 'imgs/clipboard-light.svg');
 };
+
+(() => {
+    $('#tooltip-caracal-t1').hide();
+    $('#tooltip-caracal-t2').hide();
+    $('#tooltip-caracal-t2-heavies').hide();
+    $('#tooltip-osprey-t2').hide();
+
+    $('#copy-caracal-t1').hover(tooltipHoverIn('caracal-t1'), tooltipHoverOut('caracal-t1'));
+    $('#copy-caracal-t2').hover(tooltipHoverIn('caracal-t2'), tooltipHoverOut('caracal-t2'));
+    $('#copy-caracal-t2-heavies').hover(tooltipHoverIn('caracal-t2-heavies'), tooltipHoverOut('caracal-t2-heavies'));
+    $('#copy-osprey-t2').hover(tooltipHoverIn('osprey-t2'), tooltipHoverOut('osprey-t2'));    
+})();
